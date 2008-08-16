b5f6971f303e4f326ab6da6530e695f544a3c91b
janrinze <janrinze@localhost>
2008-08-16 23:58:12 +0000

    testing some lubbock imports on the ts module

--- arch/arm/mach-pxa/loox720/loox720_ts.c
+++ arch/arm/mach-pxa/loox720/loox720_ts.c
@@ -13,8 +13,12 @@
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/delay.h>
-#include <linux/ads7846.h>
-#include <linux/touchscreen-adc.h>
+
+#include <linux/spi/spi.h>
+#include <linux/spi/ads7846.h>
+#include <asm/arch/pxa2xx_spi.h>
+
+//#include <linux/touchscreen-adc.h>
 #include <asm/irq.h>
 #include <asm/mach-types.h>
 #include <asm/hardware.h>
@@ -27,6 +31,61 @@
 
 #include "../generic.h"
 
+
+/* borrowed from lubbock.c */
+
+static struct pxa2xx_spi_master pxa_ssp_master_info = {
+	.num_chipselect	= 0,
+};
+
+static struct platform_device pxa_ssp = {
+	.name		= "pxa2xx-spi",
+	.id		= 1,
+	.dev = {
+		.platform_data	= &pxa_ssp_master_info,
+	},
+};
+
+static int loox720_ads7846_pendown_state(void)
+{
+	/* TS_BUSY is bit 8 in LUB_MISC_RD, but pendown is irq-only */
+	return 0;
+}
+
+static struct ads7846_platform_data ads_info = {
+	.model			= 7846,
+	.vref_delay_usecs	= 100,		/* internal, no cap */
+	.get_pendown_state	= loox720_ads7846_pendown_state,
+	// .x_plate_ohms		= 500,	/* GUESS! */
+	// .y_plate_ohms		= 500,	/* GUESS! */
+};
+static void ads7846_cs(u32 command)
+{
+	// no idea what this should do.. 
+
+	//static const unsigned	TS_nCS = 1 << 11;
+	//lubbock_set_misc_wr(TS_nCS, (command == PXA2XX_CS_ASSERT) ? 0 : TS_nCS);
+}
+
+static struct pxa2xx_spi_chip ads_hw = {
+	.tx_threshold		= 1,
+	.rx_threshold		= 2,
+	.cs_control		= ads7846_cs,
+};
+
+static struct spi_board_info spi_board_info[] __initdata = { {
+	.modalias	= "ads7846",
+	.platform_data	= &ads_info,
+	.controller_data = &ads_hw,
+	.irq		= LOOX720_IRQ(TOUCHPANEL_IRQ_N),
+	.max_speed_hz	= 120000 /* max sample rate at 3V */
+				* 26 /* command + data + overhead */,
+	.bus_num	= 1,
+	.chip_select	= 0,
+},
+};
+
+#if 0
 static struct ads7846_ssp_platform_data loox720_ts_ssp_params = {
     .port = 1,
     .pd_bits = 1,
@@ -67,11 +126,13 @@ static struct platform_device loox720_ts        = {
         .platform_data = &loox720_ts_params,
     }
 };
+#endif
 
 static int __devinit loox720_ts_probe(struct platform_device *dev)
 {
-    platform_device_register(&ads7846_ssp);
-    platform_device_register(&loox720_ts);
+    //platform_device_register(&ads7846_ssp);
+    //platform_device_register(&loox720_ts);
+    spi_register_board_info(spi_board_info, ARRAY_SIZE(spi_board_info));
     return 0;
 }
 
