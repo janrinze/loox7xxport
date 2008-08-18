fd2d96d09f73108640655d6caf4b8e85d7bd596f
janrinze <janrinze@localhost>
2008-08-18 13:00:27 +0000

    Additions to GPIO list and pin config.

--- include/asm-arm/arch-pxa/loox720-gpio.h
+++ include/asm-arm/arch-pxa/loox720-gpio.h
@@ -18,6 +18,11 @@
  *
  * THANKS!
  * Tomasz Figa <tom3q@staszic.waw.pl>
+ *
+ * Note (18.08.2008):	tom3q:	I have marked all the GPIOs we are sure with their directions and descriptions
+ *				if you find the meaning of some GPIO, please mark it following the scheme:
+ 				// DIR - sure - description
+ 				All the GPIOs marked as UNKNOWN are just unknown... (Don't look at their name)
  */
 
 #ifndef _LOOX720_GPIO_H_
@@ -29,42 +34,51 @@
  * GPIOs operating as Application-Specific GPIO
  */
 
-#define GPIO_NR_LOOX720_KEY_ON			0 // sure - not machine specific
-#define GPIO_NR_LOOX720_GP_RST_N		1 // sure - not machine specific
+#define GPIO_NR_LOOX720_KEY_ON			0 // IN - sure - not machine specific
+#define GPIO_NR_LOOX720_GP_RST_N		1 // IN - sure - not machine specific
+#define GPIO_NR_LOOX720_UNK_2			2 // UNKNOWN IN
+#define GPIO_NR_LOOX720_PWR_SCL			3 // UNKNOWN OUT
+#define GPIO_NR_LOOX720_PWR_SDA			4 // UNKNOWN OUT
+#define GPIO_NR_LOOX720_PWR_CAP0		5 // UNKNOWN IN
+#define GPIO_NR_LOOX720_PWR_CAP1		6 // UNKNOWN IN
+#define GPIO_NR_LOOX720_PWR_CAP2		7 // UNKNOWN IN
+#define GPIO_NR_LOOX720_PWR_CAP3		8 // UNKNOWN IN
+#define GPIO_NR_LOOX720_AC_IN_N			9 // IN - sure - low when AC Adapter is connected
 
-#define GPIO_NR_LOOX720_PWR_SCL			3
-#define GPIO_NR_LOOX720_PWR_SDA			4
-#define GPIO_NR_LOOX720_PWR_CAP0		5
-#define GPIO_NR_LOOX720_PWR_CAP1		6
-#define GPIO_NR_LOOX720_PWR_CAP2		7
-#define GPIO_NR_LOOX720_PWR_CAP3		8
-#define GPIO_NR_LOOX720_AC_IN_N			9 // sure - low when AC Adapter is connected
+#define GPIO_NR_LOOX720_BATTERY_FULL_N		11 // IN - sure - low when battery is full
+#define GPIO_NR_LOOX720_CPLD_INT		12 // IN - sure - CPLD external interrupt pin used by IRQ demuxer
+#define GPIO_NR_LOOX720_USB_DETECT_N		13 // IN - sure - detects connection to USB client controller (VBus)
+#define GPIO_NR_LOOX720_WIFI_PWR		14 // OUT - sure - CF WIFI power pin - high to enable ACX100 (must be used in pair with CPLD bit)
 
-#define GPIO_NR_LOOX720_BATTERY_FULL_N		11 // sure - low when battery is full
-#define GPIO_NR_LOOX720_CPLD_INT		12 // sure - CPLD external interrupt pin used by IRQ demuxer
-#define GPIO_NR_LOOX720_USB_DETECT_N		13 // not 100% sure - detects connection to USB client controller
-#define GPIO_NR_LOOX720_WIFI_PWR		14 // sure - CF WIFI power pin - high to enable ACX100 (must be used in pair with CPLD bit)
+#define GPIO_NR_LOOX720_WIFI_RST	 	19 // OUT - sure - CF WIFI reset pin - high to enable ACX100 reset state, low to disable
 
-#define GPIO_NR_LOOX720_WIFI_RST	 	19 // sure - CF WIFI reset pin - high to enable ACX100 reset state, low to disable
+#define GPIO_NR_LOOX720_CPU_BT_RESET_N		22 // OUT - sure - Bluetooth chip reset pin - low to enable BRF6150 reset state, high to disable
 
-#define GPIO_NR_LOOX720_CPU_BT_RESET_N		22 // sure - Bluetooth chip reset pin - low to enable BRF6150 reset state, low to disable
+#define GPIO_NR_LOOX720_UNK_36			36 // UNKNOWN OUT
 
-#define GPIO_NR_LOOX720_MMC_DETECT_N		52 // sure - low when an SD card is inserted
+#define GPIO_NR_LOOX720_UNK_38			38 // UNKNOWN IN
 
-#define GPIO_NR_LOOX720_MMC_RO			74 // sure - high when a write-protected SD card is inserted
-#define GPIO_NR_LOOX720_EARPHONE_DET_N		75 // INVALID -> please detect correct function!
+#define GPIO_NR_LOOX720_MMC_DETECT_N		52 // IN - sure - low when an SD card is inserted
 
-#define GPIO_NR_LOOX720_IR_ON_N			81
+#define GPIO_NR_LOOX720_MMC_RO			74 // IN - sure - high when a write-protected SD card is inserted
+#define GPIO_NR_LOOX720_EARPHONE_DET_N		75 // UNKNOWN OUT
 
-#define GPIO_NR_LOOX720_TOUCHPANEL_SPI_DI	86
-#define GPIO_NR_LOOX720_TOUCHPANEL_SPI_DO	87
+#define GPIO_NR_LOOX720_IR_ON_N			81 // UNKNOWN OUT
 
-#define GPIO_NR_LOOX720_FLASH_VPEN		91
+#define GPIO_NR_LOOX720_TOUCHPANEL_SPI_DI	86 // UNKNOWN OUT
+#define GPIO_NR_LOOX720_TOUCHPANEL_SPI_DO	87 // UNKNOWN IN
 
-#define GPIO_NR_LOOX720_TOUCHPANEL_IRQ_N	94
-#define GPIO_NR_LOOX720_BATT_OFF		95
-#define GPIO_NR_LOOX720_USB_CHARGE_RATE		96
+#define GPIO_NR_LOOX720_UNK_90			90 // UNKNOWN OUT
+#define GPIO_NR_LOOX720_FLASH_VPEN		91 // UNKNOWN OUT
 
-#define GPIO_NR_LOOX720_CHARGE_EN_N		107 // sure - set low to disable battery charging
+#define GPIO_NR_LOOX720_TOUCHPANEL_IRQ_N	94 // IN - sure - low when the touchscreen is being touched
+#define GPIO_NR_LOOX720_BATT_OFF		95 // UNKNOWN OUT
+#define GPIO_NR_LOOX720_USB_CHARGE_RATE		96 // UNKNOWN IN - seems like related to the sound system
+
+#define GPIO_NR_LOOX720_UNK_106			106 // UNKNOWN OUT
+#define GPIO_NR_LOOX720_CHARGE_EN_N		107 // OUT - sure - set low to disable battery charging
+
+#define GPIO_NR_LOOX720_UNK_119			119 // UNKNOWN IN
+#define GPIO_NR_LOOX720_HEADPHONE_DET		120 // IN - sure - headphone detect (high when headphones are connected)
 
 #endif /* _LOOX720_GPIO_H */
