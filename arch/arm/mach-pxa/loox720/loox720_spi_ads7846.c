46cf89e216daf246aff7f5a1d59e6e1938c8c94f
tom3q <tom3q@localhost>
2008-08-29 17:55:02 +0000

    Added pressure sensing. Fixed (in 99%) the pen up position error. Lowered sample rate to 100 Hz.

--- arch/arm/mach-pxa/loox720/loox720_spi_ads7846.c
+++ arch/arm/mach-pxa/loox720/loox720_spi_ads7846.c
@@ -20,8 +20,7 @@
 #include <asm/arch/loox720-cpld.h>
 #include <asm/arch/loox720_spi_ads7846.h>
 
-#define TS_INITIAL_DELAY	(1 * 1000000)
-#define TS_POLL_DELAY(x)	((x) * 1000000)
+#define TS_POLL_DELAY(x)	(((x) - 1) * 1000000) // sample rate = +/- 5Hz with 200kHz spi bus
 
 // =======================================================
 // hardcoded values for testing
@@ -39,12 +38,16 @@
 	ads7846 commands 
    ========================================================== */
 
-static loox720_ads7846_spi_message ads_cmds[5] = {
-	{ .cmd = 0xD3<<7, }, // setup x 
-	{ .cmd = 0xD0<<7, }, // read x 
-	{ .cmd = 0x93<<7, }, // setup y
-	{ .cmd = 0x90<<7, }, // read y
-	{ .cmd = 0x0000, }  // powerdown
+static loox720_ads7846_spi_message ads_cmds[] = {
+	{ .cmd = READ_X(1)<<7, }, // setup x 
+	{ .cmd = READ_X(0)<<7, }, // read x 
+	{ .cmd = READ_Y(1)<<7, }, // setup y
+	{ .cmd = READ_Y(0)<<7, }, // read y
+	{ .cmd = READ_Z1(1)<<7, }, // read z1
+	{ .cmd = READ_Z2(1)<<7, }, // read z2
+	{ .cmd = READ_X(1)<<7, },  // power down
+	{ .cmd = READ_X(0)<<7, },
+	{ .cmd = PWRDOWN<<7, },
 };
 
 /* ==========================================================
@@ -55,7 +58,7 @@ int SPI_read_write_block(loox720_ads7846_device_info * dev, loox720_ads7846_spi_
 {
 	int i;
 	struct spi_transfer *x = &dev->xfer[0];
-	struct spi_message  *m = &dev->msg[0];
+	struct spi_message  *m = &dev->msg;
 
 	// copy the messages to our block
 	for (i=0;i<num_commands;i++)
@@ -64,7 +67,7 @@ int SPI_read_write_block(loox720_ads7846_device_info * dev, loox720_ads7846_spi_
 	spi_message_init(m);
 	x->tx_buf = &dev->sendblock[0];
 	x->rx_buf = &dev->receiveblock[0];
-	x->len = num_commands*sizeof(dev->sendblock[0]); // 5 messages
+	x->len = num_commands*sizeof(dev->sendblock[0]); // 7 messages
 	spi_message_add_tail(x, m);
 	
 	m->complete = callback;
@@ -100,12 +103,16 @@ void loox720_ads7846_report(loox720_ads7846_device_info * dev, unsigned int Rt, 
 static void loox720_ads7846_callback(void *data)
 {
 	loox720_ads7846_device_info * ads = data;
-	int x,y /*,z1,z2*/;
+	int x, y, z1, z2;
+	
+	if(ads->x > 10 && ads->y > 10)
+		if(!readpen(ads))
+			loox720_ads7846_report(ads, ads->rt, ads->x ,ads->y);
 
 	x  = convert_data_12( ads, 1);
 	y  = convert_data_12( ads, 3 );
-//	z1 = convert_data_12( ads, 0 );
-//	z2 = convert_data_12( ads, 2 );
+	z1 = convert_data_12( ads, 4 );
+	z2 = convert_data_12( ads, 5 );
 	
 	if ((x>10)&&(y>10)) // valid measurement
 	{
@@ -126,19 +133,29 @@ static void loox720_ads7846_callback(void *data)
 		if (x<0) x=0;if (x>479) x=479;
 		if (y<0) y=0;if (y>639) y=639;
 #endif
-		ads->oldx = ads->x;
-		ads->oldy = ads->y;
+		if (likely(x && z1)) {
+			/* compute touch pressure resistance using equation #2 */
+			ads->rt = z2;
+			ads->rt -= z1;
+			ads->rt *= x;
+			ads->rt *= 400;
+			ads->rt /= z1;
+			ads->rt = (ads->rt + 2047) >> 12;
+		} else
+			ads->rt = 0;
+		
 		ads->x=x;
 		ads->y=y;		
-		loox720_ads7846_report(ads, MAX_12BIT -1, ads->x ,ads->y);
 	}
 	
-	if (ads->pendown) {
+	if (!readpen(ads)) {
 		hrtimer_start(&ads->timer, ktime_set(0, TS_POLL_DELAY(ads->sample_rate)),
 			      HRTIMER_MODE_REL);
 	} else {
 		// report pen up
-		loox720_ads7846_report(ads, 0, ads->oldx, ads->oldy);
+		//loox720_ads7846_report(ads, 0, ads->x, ads->y);
+		ads->x = 0;
+		ads->y = 0;
 		input_report_key(ads->input, BTN_TOUCH, 0);
 		input_report_abs(ads->input, ABS_PRESSURE, 0);
 		input_sync(ads->input);
@@ -168,11 +185,13 @@ static enum hrtimer_restart loox720_ads7846_timer(struct hrtimer *handle)
 {
 	loox720_ads7846_device_info *ads = container_of(handle, loox720_ads7846_device_info, timer);
 	
-	if(ads->pendown) {
+	if(!readpen(ads)) {
 		read_loox720_ads7846(ads);
 	} else {
 		// report pen up
-		loox720_ads7846_report(ads, 0, ads->oldx, ads->oldy);
+//		loox720_ads7846_report(ads, 0, ads->oldx, ads->oldy);
+		ads->x = 0;
+		ads->y = 0;
 		input_report_key(ads->input, BTN_TOUCH, 0);
 		input_report_abs(ads->input, ABS_PRESSURE, 0);
 		input_sync(ads->input);
@@ -190,27 +209,14 @@ static void loox720_ads7846_work(struct work_struct *work)
 {
 	loox720_ads7846_device_info * ads = container_of(work, loox720_ads7846_device_info, work);
 	
-	if (ads->busy==0)
-	{
-		ads->busy=1;
-		// report pen down
-		ads->report_button=1;
-		read_loox720_ads7846(ads);
-	}
+	// report pen down
+	ads->report_button=1;
+	read_loox720_ads7846(ads);
 }
 
 /* ==========================================================
 	interrupt routines
    ========================================================== */
-
-static inline unsigned int readpen(loox720_ads7846_device_info *dev)	\
-{										\
-	if(dev)									\
-		return (gpio_get_value(dev->pen )?1:0);			\
-	printk( KERN_INFO "ADS7846 trying to read data with empty dev\n");	\
-	return 0;								\
-}
-
 
 irqreturn_t pendown_interrupt(int irq, void *data)
 {
@@ -218,23 +224,20 @@ irqreturn_t pendown_interrupt(int irq, void *data)
 	
 	if (readpen(ads)==0)
 	{
-		ads->pendown=1;
 		if (ads->busy==0)
 		{
+			ads->busy=1;
 			schedule_work(&ads->work);
 		}
-	} else {
-		ads->pendown=0;
 	}
    
     return IRQ_HANDLED;
 }
 
 /* ==========================================================
-
 	module init and exit routines
-
    ========================================================== */
+
 static char * myname="loox720_spi_ads7846";
 
 static int __init loox720_ads7846_probe(struct spi_device *spi)
@@ -265,10 +268,11 @@ static int __init loox720_ads7846_probe(struct spi_device *spi)
 	ads->xmax = 3610;
 	ads->ymin = 333;
 	ads->ymax = 3780;
+	ads->pen_recheck_usecs = 100;
 	
 	ads->pen = GPIO_NR_LOOX720_TOUCHPANEL_IRQ_N;
 	ads->irq = IRQ_GPIO(GPIO_NR_LOOX720_TOUCHPANEL_IRQ_N);
-	ads->sample_rate = 5; // delay in ms
+	ads->sample_rate = 10; // delay in ms
 	
 	err = spi_setup(spi);
 	if (err < 0)
@@ -304,7 +308,7 @@ static int __init loox720_ads7846_probe(struct spi_device *spi)
 	ads->input = input_dev;
 
 	if (request_irq(ads->irq, pendown_interrupt,
-			IRQF_TRIGGER_FALLING|IRQF_TRIGGER_RISING,
+			IRQF_TRIGGER_FALLING,
 			myname, ads)) {
 		printk(KERN_ERR "Failed to request loox720 ads7846 IRQ\n");
 		err = -ENOMEM;
@@ -355,4 +359,3 @@ module_exit(loox720_ads7846_exit);
 MODULE_AUTHOR ("Jan Rinze Peterzon & Tomasz Figa");
 MODULE_DESCRIPTION ("SPI Touchscreen support for PocketLOOX 700 Series");
 MODULE_LICENSE ("GPL");
-
--- include/asm-arm/arch-pxa/loox720_spi_ads7846.h
+++ include/asm-arm/arch-pxa/loox720_spi_ads7846.h
@@ -1,12 +1,50 @@
 
 /*--------------------------------------------------------------------------*/
-
-/*
-	The ADS7845 has touchscreen and other sensors.
+/* The ADS7846 has touchscreen and other sensors.
+ * Earlier ads784x chips are somewhat compatible.
  */
+#define	ADS_START		(1 << 7)
+#define	ADS_A2A1A0_d_y		(1 << 4)	/* differential */
+#define	ADS_A2A1A0_d_z1		(3 << 4)	/* differential */
+#define	ADS_A2A1A0_d_z2		(4 << 4)	/* differential */
+#define	ADS_A2A1A0_d_x		(5 << 4)	/* differential */
+#define	ADS_A2A1A0_temp0	(0 << 4)	/* non-differential */
+#define	ADS_A2A1A0_vbatt	(2 << 4)	/* non-differential */
+#define	ADS_A2A1A0_vaux		(6 << 4)	/* non-differential */
+#define	ADS_A2A1A0_temp1	(7 << 4)	/* non-differential */
+#define	ADS_8_BIT		(1 << 3)
+#define	ADS_12_BIT		(0 << 3)
+#define	ADS_SER			(1 << 2)	/* non-differential */
+#define	ADS_DFR			(0 << 2)	/* differential */
+#define	ADS_PD10_PDOWN		(0 << 0)	/* lowpower mode + penirq */
+#define	ADS_PD10_ADC_ON		(1 << 0)	/* ADC on */
+#define	ADS_PD10_REF_ON		(2 << 0)	/* vREF on + penirq */
+#define	ADS_PD10_ALL_ON		(3 << 0)	/* ADC + vREF on */
 
 #define	MAX_12BIT	((1<<12)-1)
 
+/* leave ADC powered up (disables penirq) between differential samples */
+#define	READ_12BIT_DFR(x, adc, vref) (ADS_START | ADS_A2A1A0_d_ ## x \
+	| ADS_12_BIT | ADS_DFR | \
+	(adc ? ADS_PD10_ADC_ON : 0) | (vref ? ADS_PD10_REF_ON : 0))
+
+#define	READ_X(vref)	(READ_12BIT_DFR(x,  1, vref))
+#define	READ_Y(vref)	(READ_12BIT_DFR(y,  1, vref))
+#define	READ_Z1(vref)	(READ_12BIT_DFR(z1, 1, vref))
+#define	READ_Z2(vref)	(READ_12BIT_DFR(z2, 1, vref))
+
+#define	PWRDOWN		(READ_12BIT_DFR(y,  0, 0))	/* LAST */
+
+/* single-ended samples need to first power up reference voltage;
+ * we leave both ADC and VREF powered
+ */
+#define	READ_12BIT_SER(x) (ADS_START | ADS_A2A1A0_ ## x \
+	| ADS_12_BIT | ADS_SER)
+
+#define	REF_ON	(READ_12BIT_DFR(x, 1, 1))
+#define	REF_OFF	(READ_12BIT_DFR(y, 0, 0))
+
+#define	MAX_12BIT	((1<<12)-1)
 
 typedef struct loox720_ads7846_spi_message
 {
@@ -16,19 +54,27 @@ typedef struct loox720_ads7846_spi_message
 
 typedef struct loox720_ads7846_device_info {
 	unsigned int 		irq,pen;
-	volatile unsigned int 	x, y, oldx, oldy;
+	volatile unsigned int 	x, y, oldx, oldy, rt;
 	unsigned int		xmin, xmax, ymin, ymax;
 	unsigned int 		sample_rate;
+	unsigned int		pen_recheck_usecs;
 	volatile int		busy;
-	volatile int		pendown;
+//	volatile int		pendown;
 	void * 			input;
 	struct spi_device 	*spi;
-	struct spi_transfer	xfer[5];
-	struct spi_message	msg[5];
-	unsigned short 		sendblock[10] __attribute__ ((aligned (8)));
-	unsigned short		receiveblock[10] __attribute__ ((aligned (8)));
+	struct spi_transfer	xfer[9];
+	struct spi_message	msg;
+	unsigned short 		sendblock[18];
+	unsigned short		receiveblock[18];
 	unsigned int		report_button;
 	struct hrtimer timer;
 	struct work_struct work;	
 } loox720_ads7846_device_info;
 
+static inline unsigned int readpen(loox720_ads7846_device_info *dev)	\
+{										\
+	if(dev)									\
+		return (gpio_get_value(dev->pen )?1:0);			\
+	printk( KERN_INFO "ADS7846 trying to read data with empty dev\n");	\
+	return 0;								\
+}
